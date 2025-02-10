7d6a8c76bfd4b2e26fad2faf37f767af3f0257fa
iateacatandawhalen <96869531+iateacatandawhalen@users.noreply.github.com>
2025-02-10 14:37:30 +0100

    Update SyntaxMarkdown.pas
    Hopefully this file won't be a big pain in the ass later on

--- SyntaxMarkdown.pas
+++ SyntaxMarkdown.pas
@@ -1,51 +1,28 @@
 unit SyntaxMarkdown;
 
 interface
-procedure PrintMarkdownSyntaxHighlighting(line: string);
 
-implementation
-uses crt, SysUtils;
+uses
+  SysUtils, Classes;
 
 const
-  MarkdownKeywords: array[0..5] of string = ('#', '##', '###', '**', '_', '`');
+  MarkdownElements: array[0..3] of string = (
+    '#', '*', '**', '```'
+  );
 
-procedure PrintMarkdownSyntaxHighlighting(line: string);
-var
-  i, j: Integer;
-  word: string;
-  isKeyword: Boolean;
+procedure HighlightMarkdownSyntax(const Line: string; var HighlightedLine: string);
+
+implementation
+
+procedure HighlightMarkdownSyntax(const Line: string; var HighlightedLine: string);
 begin
-  i := 1;
-  while i <= Length(line) do
-  begin
-    word := '';
-    
-    while (i <= Length(line)) and (line[i] <> ' ') do
-    begin
-      word := word + line[i];
-      Inc(i);
-    end;
-
-    isKeyword := False;
-    for j := 0 to High(MarkdownKeywords) do
-    begin
-      if word = MarkdownKeywords[j] then
-      begin
-        TextColor(LightBlue);
-        Write(word);
-        TextColor(White);
-        isKeyword := True;
-        Break;
-      end;
-    end;
-
-    if not isKeyword then
-      Write(word);
-    
-    Write(' ');
-    Inc(i);
-  end;
-  WriteLn;
+  HighlightedLine := Line; // Simple implementation for Markdown
+  if Pos('#', Line) = 1 then
+    HighlightedLine := '[Header]' + Line + '[/Header]'
+  else if Pos('```', Line) > 0 then
+    HighlightedLine := '[CodeBlock]' + Line + '[/CodeBlock]'
+  else if Pos('*', Line) > 0 then
+    HighlightedLine := '[Italic]' + Line + '[/Italic]';
 end;
 
 end.
